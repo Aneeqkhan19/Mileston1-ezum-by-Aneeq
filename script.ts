5f06736756bc3f67a0121a0b71c78b83c3cfe213
Aneeqkhan19 <Aneeqkhankhan41@gmail.com>
2024-09-08 10:29:01 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,15 @@
+// listing element
+document.getElementByIdById('resumeform')?.addEventListener('submit', function(event) {
+    event.preventDefult();
+
+
+//type assertion
+const nameElement = document.getElementById('name') as HTMLImageElement;
+const emailElement = document.getElementById('email') as HTMLImageElement;
+const phoneElement = document.getElementById('phone') as HTMLImageElement;
+const educationElement = document.getElementById('education') as HTMLImageElement;
+const experienceElement = document.getElementById('experience') as HTMLImageElement;
+const skillsElement = document.getElementById('skills') as HTMLImageElement;
+
+})
+
